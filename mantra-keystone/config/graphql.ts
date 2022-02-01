d7d7c616392e3801859825faf8cddf0f6973b114
mrkutly <mark.sauer.utley@gmail.com>
2022-02-01 18:30:04 -0500

    use introspection and playground in production

---
+++ mantra-keystone/config/graphql.ts
@@ -0,0 +1,11 @@
+import { BaseKeystoneTypeInfo, GraphQLConfig } from "@keystone-6/core/types";
+import { Env } from "../types";
+
+export const GQLConfig: Record<Env, GraphQLConfig> = {
+  [Env.development]: {},
+  [Env.production]: {
+    playground: true,
+    apolloConfig: { introspection: true }
+  }
+}
+
