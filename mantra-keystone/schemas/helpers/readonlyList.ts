abb2a501a287f6795354a7fe62565a07931f46e5
Mark Sauer-Utley <mark.sauer.utley@almanac.io>
2022-02-01 20:41:58 -0500

    fix auth check

--- mantra-keystone/schemas/helpers/readonlyList.ts
+++ mantra-keystone/schemas/helpers/readonlyList.ts
@@ -1,7 +1,7 @@
 import { list, ListConfig } from '@keystone-6/core';
 import { BaseFields, BaseListTypeInfo } from '@keystone-6/core/types';
 
-const isLoggedIn = ({ session }: { session: any }) => session?.data;
+const isLoggedIn = ({ session }: { session: any }) => !!session?.data;
 
 export const readonlyList = (opts: ListConfig<BaseListTypeInfo, BaseFields<BaseListTypeInfo>>) => {
   const access = {
