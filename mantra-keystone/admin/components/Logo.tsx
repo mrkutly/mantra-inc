de6e1472c9c436633ac4aaab33c9b93aa40234f4
Mark Sauer-Utley <mark.sauer.utley@almanac.io>
2022-02-01 21:34:02 -0500

    remove text decoration on link

--- mantra-keystone/admin/components/Logo.tsx
+++ mantra-keystone/admin/components/Logo.tsx
@@ -8,10 +8,12 @@ export const Logo = () => {
     background: 'linear-gradient(to right, rgb(0, 0, 0), rgb(185, 185, 185))',
     backgroundClip: 'text',
     WebkitTextFillColor: 'transparent',
+    textDecoration: 'none',
     ":hover": {
       background: 'linear-gradient(90deg, #EE2F9E 0%, #BC2FEE 100%)',
       backgroundClip: 'text',
       WebkitTextFillColor: 'transparent',
+      textDecoration: 'none',
     }
   }}
     href="https://mantrapercussion.org"
